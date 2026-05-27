93e044f37818d76d10949ca3343c64715f5ec8cf
Yi <xiangyi.8@bytedance.com>
2026-05-26 20:00:28 -0700

    Remove Expo starter content

--- src/components/app-tabs.web.tsx
+++ src/components/app-tabs.web.tsx
@@ -55,7 +55,7 @@ export function CustomTabList(props: TabListProps) {
     <View {...props} style={styles.tabListContainer}>
       <ThemedView type="backgroundElement" style={styles.innerContainer}>
         <ThemedText type="smallBold" style={styles.brandText}>
-          Expo Starter
+          Voice
         </ThemedText>
 
         {props.children}
